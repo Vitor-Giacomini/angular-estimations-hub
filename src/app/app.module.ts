d2a27d82781df9436321966e1a35316796614198
Vitor-Giacomini <vitorgdgiacomini@gmail.com>
2023-01-28 21:11:05 -0300

    Re-organize files into modules and components, add routing and change navbar and footer

---
+++ src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FooterComponent } from './modules/core/footer/footer.component';
+import { NavbarComponent } from './modules/core/navbar/navbar.component';
+import { HomeComponent } from './modules/feature/home/home.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/modules/core/core.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CoreRoutingModule } from './core-routing.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+
+
+@NgModule({
+  declarations: [
+    NavbarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    CoreRoutingModule
+  ],
+  exports: [
+    NavbarComponent,
+    FooterComponent
+  ]
+})
+export class CoreModule { }
