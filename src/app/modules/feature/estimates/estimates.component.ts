b5cd9f8965503a763526b03eadfe5a8c43f05bbd
Vitor-Giacomini <vitorgdgiacomini@gmail.com>
2023-01-30 11:44:28 -0300

    Added tabs that hide their content when closed to Estimates Page, added Icons using Font Awesome Icons Pack

--- src/app/modules/feature/estimates/estimates.component.ts
+++ src/app/modules/feature/estimates/estimates.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-estimates',
   templateUrl: './estimates.component.html',
   styleUrls: ['./estimates.component.scss']
 })
-export class EstimatesComponent {
+export class EstimatesComponent implements OnInit{
+
+  ngOnInit(): void {
+
+  }
+
+  selectedTab: String = "all-estimates";
+
+  setTab(tabName: String): void {
+    this.selectedTab = tabName;
+  }
 
 }
