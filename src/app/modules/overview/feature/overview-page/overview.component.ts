16a4ec99bb8b3d9ff02392a81f35ca33b4d93b5d
Vitor-Giacomini <vitorgdgiacomini@gmail.com>
2023-07-08 18:55:26 -0300

    feat(overview): add info panels

--- src/app/modules/overview/feature/overview-page/overview.component.ts
+++ src/app/modules/overview/feature/overview-page/overview.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
+import { HttpErrorResponse } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { startWith, catchError, EMPTY, ignoreElements, of, combineLatest } from 'rxjs';
+import { Estimation } from 'src/app/modules/estimations/@models/estimation.model';
+import { EstimationService } from 'src/app/modules/estimations/data-access/estimation.service';
+import { Product } from 'src/app/modules/products/@models/product.model';
+import { ProductService } from 'src/app/modules/products/data-access/product.service';
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-export class OverviewComponent {
+export class OverviewComponent implements OnInit{
 
+  constructor(private estimationService: EstimationService, private productService: ProductService) { }
+
+  estimationList!: Estimation[];
+  productList!: Product[];
+
+  ngOnInit(): void {
+    this.estimationService.getEstimations().subscribe((estimations) => { this.estimationList = estimations });
+    this.productService.getProducts().subscribe((products) => { this.productList = products });
+  }
+
+  getTotalSavings(){
+    let totalSavings = 0;
+    this.estimationList.forEach(estimation => {
+      if (estimation.estimationStatus === 'accepted') {
+        totalSavings += estimation.estimationSavings;
+      }
+    });
+    return totalSavings;
+  }
+
+  getApprovedEstimations(){
+    let approvedEstimations = 0;
+    this.estimationList.forEach(estimation => {
+      if (estimation.estimationStatus === 'accepted') {
+        approvedEstimations++;
+      }
+    });
+    return approvedEstimations;
+  }
+
+  findBiggestEstimation(){
+    let maxSavings = 0;
+    this.estimationList.forEach((estimation) => {
+      if (estimation.estimationSavings > maxSavings) {
+        maxSavings = estimation.estimationSavings;
+      }
+    });
+    return maxSavings;
+  }
 }
