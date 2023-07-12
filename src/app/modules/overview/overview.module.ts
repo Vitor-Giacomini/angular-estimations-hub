78c9905c26a84b07fb4a88b199ac84fe86085ec9
Vitor-Giacomini <vitorgdgiacomini@gmail.com>
2023-07-11 21:04:47 -0300

    fix(overview): removed unused imports

--- src/app/modules/overview/overview.module.ts
+++ src/app/modules/overview/overview.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { OverviewRoutingModule } from './overview-routing.module';
-import { NavbarComponent } from 'src/app/layout/ui/navbar/navbar.component';
-import { FooterComponent } from 'src/app/layout/ui/footer/footer.component';
 
 
 @NgModule({
