08f15ac578ba9087ef392032117dd6b52928ec03
Vitor-Giacomini <vitorgdgiacomini@gmail.com>
2023-07-11 21:04:03 -0300

    refactor(overview): add viewModel and remove subscriptions

--- src/app/modules/overview/feature/overview-page/overview.component.ts
+++ src/app/modules/overview/feature/overview-page/overview.component.ts
@@ -1,6 +1,6 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { startWith, catchError, EMPTY, ignoreElements, of, combineLatest } from 'rxjs';
+import { startWith, catchError, EMPTY, ignoreElements, of, combineLatest, map, tap } from 'rxjs';
 import { Estimation } from 'src/app/modules/estimations/@models/estimation.model';
 import { EstimationService } from 'src/app/modules/estimations/data-access/estimation.service';
 import { Product } from 'src/app/modules/products/@models/product.model';
@@ -11,51 +11,64 @@ import { ProductService } from 'src/app/modules/products/data-access/product.ser
   templateUrl: './overview.component.html',
   styleUrls: ['./overview.component.scss']
 })
-export class OverviewComponent implements OnInit{
+export class OverviewComponent{
 
-  constructor(private estimationService: EstimationService, private productService: ProductService) { }
+  constructor(private estimationService: EstimationService) { }
 
   estimationList!: Estimation[];
-  productList!: Product[];
   totalSavings: number = 0;
   approvedEstimations: number = 0;
   biggestEstimation: number = 0;
 
-  ngOnInit(): void {
-    this.estimationService.getEstimations().subscribe((estimations) => { this.estimationList = estimations });
-    this.productService.getProducts().subscribe((products) => { this.productList = products });
-  }
+  estimations$ = this.estimationService.getEstimations().pipe(
+    startWith(null),
+    catchError(() => EMPTY)
+  );
+  estimationsError$ = this.estimationService.getEstimations().pipe(
+    ignoreElements(),
+    startWith(null),
+    catchError(error => {
+      error = new HttpErrorResponse({ status: 404, statusText: 'Could not load estimations' });
+      return of(error);
+    })
+  )
+  viewModel$ = combineLatest({
+    estimations: this.estimations$,
+    estimationsError: this.estimationsError$
+  }).pipe(
+    tap(({estimations}) => {
+      if (estimations) {
+        this.getTotalSavings(estimations);
+        this.getApprovedEstimations(estimations);
+        this.findBiggestEstimation(estimations);
+      }
+    })
+  );
 
-  getTotalSavings(){
-    let totalSavings = 0;
-    this.estimationList.forEach(estimation => {
+  getTotalSavings(estimations: Estimation[]){
+    console.log(estimations);
+    estimations.forEach(estimation => {
       if (estimation.estimationStatus === 'accepted') {
-        totalSavings += estimation.estimationSavings;
+        this.totalSavings += estimation.estimationSavings;
       }
     });
-    this.totalSavings = totalSavings;
-    return totalSavings;
   }
 
-  getApprovedEstimations(){
-    let approvedEstimations = 0;
-    this.estimationList.forEach(estimation => {
+  getApprovedEstimations(estimations: Estimation[]){
+    estimations.forEach(estimation => {
       if (estimation.estimationStatus === 'accepted') {
-        approvedEstimations++;
+        this.approvedEstimations++;
       }
     });
-    this.approvedEstimations = approvedEstimations;
-    return approvedEstimations;
   }
 
-  findBiggestEstimation(){
+  findBiggestEstimation(estimations: Estimation[]){
     let maxSavings = 0;
-    this.estimationList.forEach((estimation) => {
+    estimations.forEach((estimation) => {
       if (estimation.estimationSavings > maxSavings) {
         maxSavings = estimation.estimationSavings;
       }
     });
     this.biggestEstimation = maxSavings;
-    return maxSavings;
   }
 }
