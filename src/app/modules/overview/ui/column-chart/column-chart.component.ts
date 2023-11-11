49c31ef00e2ac5bd6365878c7668dbbe77fb130b
Vitor-Giacomini <vitorgdgiacomini@gmail.com>
2023-11-11 16:18:36 -0300

    feat(column-chart): populate chart with actual data and customize colors

--- src/app/modules/overview/ui/column-chart/column-chart.component.ts
+++ src/app/modules/overview/ui/column-chart/column-chart.component.ts
@@ -2,6 +2,7 @@ import { Component, Input, SimpleChanges } from '@angular/core';
 import { Estimation } from 'src/app/modules/estimations/@models/estimation.model';
 import Chart from 'chart.js/auto';
 import ChartDataLabels from 'chartjs-plugin-datalabels';
+import { Product } from 'src/app/modules/products/@models/product.model';
 
 @Component({
   selector: 'app-column-chart',
@@ -10,10 +11,21 @@ import ChartDataLabels from 'chartjs-plugin-datalabels';
 })
 export class ColumnChartComponent {
   public chart: any;
+
+  @Input() products!: Product[];
   @Input() estimations!: Estimation[];
-  pendingEstimations: Estimation[] = [];
-  approvedEstimations: Estimation[] = [];
-  rejectedEstimations: Estimation[] = [];
+
+  chairEstimations: Estimation[] = [];
+  tableEstimations: Estimation[] = [];
+  stoolEstimations: Estimation[] = [];
+
+  chairSavings: number = 0;
+  tableSavings: number = 0;
+  stoolSavings: number = 0;
+
+  chairCombinedPrice = 220;
+  tableCombinedPrice = 280;
+  stoolCombinedPrice = 160;
 
   ngOnChanges(changes: SimpleChanges) {
     if (changes['estimations'] && changes['estimations'].currentValue) {
@@ -23,42 +35,60 @@ export class ColumnChartComponent {
 
   mapEstimations(){
     this.estimations.forEach(estimation => {
-      if(estimation.estimationStatus !== "proposed"){
-        estimation.estimationStatus === "accepted" 
-          ? this.approvedEstimations.push(estimation) 
-          : this.rejectedEstimations.push(estimation)
+      if (estimation.productName.includes("Poltrona") || estimation.productName.includes("Assento")) {
+        this.chairEstimations.push(estimation);
+      } else if (estimation.productName.includes("Mesa")) {
+        this.tableEstimations.push(estimation);
+      } else if (estimation.productName.includes("Banco")) {
+        this.stoolEstimations.push(estimation);
       }
-      else{
-        this.pendingEstimations.push(estimation);
-      }
-    })
+    });    
+    this.getSavings();
     this.createChart();
   }
 
+  getSavings(){
+    this.chairSavings = this.chairEstimations.reduce((total, estimation) => {
+      return estimation.estimationStatus === "accepted" ? total + estimation.estimationSavings : total;
+    }, 0);
+  
+    this.tableSavings = this.tableEstimations.reduce((total, estimation) => {
+      return estimation.estimationStatus === "accepted" ? total + estimation.estimationSavings : total;
+    }, 0);
+  
+    this.stoolSavings = this.stoolEstimations.reduce((total, estimation) => {
+      return estimation.estimationStatus === "accepted" ? total + estimation.estimationSavings : total;
+    }, 0); 
+  }
+  
+
   createChart(){
     if (this.chart) {
-      this.chart.destroy(); // Destroy the previous instance of the chart if it exists
+      this.chart.destroy(); 
     }
   
-    this.chart = new Chart("columnChart", { // Make sure to have a canvas with this ID in your template
+    this.chart = new Chart("columnChart", { 
       type: 'bar',
       data: {
-        labels: ['Dataset 1', 'Dataset 2'], // These would be your actual data labels
+        labels: ['Chairs', 'Tables', 'Stools'], 
         datasets: [{
-          label: 'Front Bar',
-          data: [10, 20], // Replace with actual data
-          backgroundColor: 'rgba(255, 99, 132, 0.2)',
-          borderColor: 'rgba(255, 99, 132, 1)',
+          label: 'Cost - Savings',
+          data: [
+            this.chairCombinedPrice - this.chairSavings, 
+            this.tableCombinedPrice - this.tableSavings, 
+            this.stoolCombinedPrice - this.stoolSavings],
+          backgroundColor: 'rgba(46, 204, 113, 0.5)',
+          borderColor: 'rgba(46, 204, 113, 1)',
           borderWidth: 1,
           categoryPercentage: 0.8,
           barPercentage: 0.9,
           order: 1
         },
         {
-          label: 'Back Bar',
-          data: [15, 25], // Replace with actual data
-          backgroundColor: 'rgba(54, 162, 235, 0.2)',
-          borderColor: 'rgba(54, 162, 235, 1)',
+          label: 'Original Cost',
+          data: [this.chairCombinedPrice, this.tableCombinedPrice, this.stoolCombinedPrice], 
+          backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          borderColor: 'rgba(255, 99, 132, 1)',
           borderWidth: 1,
           categoryPercentage: 0.8,
           barPercentage: 0.9,
@@ -76,18 +106,34 @@ export class ColumnChartComponent {
               drawOnChartArea: false,
               drawTicks: false,
             },
-            offset: true
+            offset: true,
+            ticks: {
+              font: {
+                size: 20
+              }
+            }
           },
           y: {
-            stacked: false
+            stacked: false,
+            ticks: {
+              font: {
+                size: 16
+              }
+            }
           }
         },
         plugins: {
           datalabels: {
-            // Configure your datalabels as needed
+            
           },
           legend: {
-            display: true
+            display: true,
+            labels: {
+              padding: 20,
+              font: {
+                size: 20
+              }
+            }
           }
         }
       },
