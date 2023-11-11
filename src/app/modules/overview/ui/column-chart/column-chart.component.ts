937a09489fd621946dff756d133c6b24e9411bd8
Vitor-Giacomini <vitorgdgiacomini@gmail.com>
2023-11-11 14:25:32 -0300

    feat(column-chart): add sample chart

--- src/app/modules/overview/ui/column-chart/column-chart.component.ts
+++ src/app/modules/overview/ui/column-chart/column-chart.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, SimpleChanges } from '@angular/core';
 import { Estimation } from 'src/app/modules/estimations/@models/estimation.model';
+import Chart from 'chart.js/auto';
+import ChartDataLabels from 'chartjs-plugin-datalabels';
 
 @Component({
   selector: 'app-column-chart',
@@ -34,6 +36,69 @@ export class ColumnChartComponent {
   }
 
   createChart(){
-    
+    if (this.chart) {
+      this.chart.destroy(); // Destroy the previous instance of the chart if it exists
+    }
+  
+    this.chart = new Chart("columnChart", { // Make sure to have a canvas with this ID in your template
+      type: 'bar',
+      data: {
+        labels: ['Dataset 1', 'Dataset 2'], // These would be your actual data labels
+        datasets: [{
+          label: 'Front Bar',
+          data: [10, 20], // Replace with actual data
+          backgroundColor: 'rgba(255, 99, 132, 0.2)',
+          borderColor: 'rgba(255, 99, 132, 1)',
+          borderWidth: 1,
+          categoryPercentage: 0.8,
+          barPercentage: 0.9,
+          order: 1
+        },
+        {
+          label: 'Back Bar',
+          data: [15, 25], // Replace with actual data
+          backgroundColor: 'rgba(54, 162, 235, 0.2)',
+          borderColor: 'rgba(54, 162, 235, 1)',
+          borderWidth: 1,
+          categoryPercentage: 0.8,
+          barPercentage: 0.9,
+          order: 2
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        scales: {
+          x: {
+            stacked: false,
+            grid: {
+              display: false,
+              drawOnChartArea: false,
+              drawTicks: false,
+            },
+            offset: true
+          },
+          y: {
+            stacked: false
+          }
+        },
+        plugins: {
+          datalabels: {
+            // Configure your datalabels as needed
+          },
+          legend: {
+            display: true
+          }
+        }
+      },
+      plugins: [ChartDataLabels]
+    });
+  
+    // You will need to adjust your approach to update or configure the chart as manipulating _model is not recommended.
+    // The recommended approach is to update the chart data and then call this.chart.update();
+  
+    this.chart.update(); // Update the chart to render the shifted bars
   }
+  
+  
 }
